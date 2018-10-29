308093bfe49479059fb4d49f2fabfaf1e59b935d
deventorum <deventorum@gmail.com>
2018-10-29 15:51:16 +0000

    Step Print in Chunks finished

---
+++ step1.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+	const requestOptions = {
+		host: 'sytantris.github.io',
+		path: '/http-examples/step1.html'
+	};
+  
+	https.get(requestOptions, function(response) {
+		response.on('data', function(data) {
+			console.log(data + '\n');
+		});
+		response.on('end', function() {
+			console.log('Response stream complete.');
+		});
+	});
+
+}
+
+getAndPrintHTMLChunks();
